ea166f17214c17a5b64de5b11c1e8b966d13cbcf
hazzajenko <jenkinsh1@outlook.com>
2022-05-24 20:02:27 +1000

    v0.2

--- BlazorChatSignalR/Server/Hubs/ChatHub.cs
+++ BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -4,10 +4,19 @@ namespace BlazorChatSignalR.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
         public override async Task OnConnectedAsync()
         {
-            await AddMessageToChat("", "User connected");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat((String.Empty), $"{username} joined the chat!");
             await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left the chat!");
         }
 
         public async Task AddMessageToChat (string user, string message)
