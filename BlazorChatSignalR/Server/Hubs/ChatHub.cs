019bba272b2e816d5fe08980e97478b39c721d1a
hazzajenko <jenkinsh1@outlook.com>
2022-05-24 19:29:40 +1000

    v0.1

--- BlazorChatSignalR/Server/Hubs/ChatHub.cs
+++ BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -1,6 +1,18 @@
-﻿namespace BlazorChatSignalR.Server.Hubs
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatSignalR.Server.Hubs
 {
-    public class ChatHub
+    public class ChatHub : Hub
     {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "User connected");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task AddMessageToChat (string user, string message)
+        {
+            await Clients.All.SendAsync("Received message", user, message);
+        }
     }
 }
